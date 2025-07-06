7f51d930c7d8bbc0728683de1c80210f98a3ff62
R.Keelan <R.Keelan@gmail.com>
2025-07-06 10:20:05 -0400

    Rename image data to default image data (in preparation for sending images to the Device)

--- Device/Core/Src/main.c
+++ Device/Core/Src/main.c
@@ -60,8 +60,8 @@ UART_HandleTypeDef huart6;
 SDRAM_HandleTypeDef hsdram1;
 
 /* USER CODE BEGIN PV */
-const uint16_t image_data[] = {
-    #include "image_rgb565.h"
+const uint16_t default_image_data[] = {
+    #include "default_image_rgb565.h"
 };
 /* USER CODE END PV */
 
@@ -130,25 +130,25 @@ int main(void)
   MX_USART6_UART_Init();
   /* USER CODE BEGIN 2 */
 
-  // Display image on LCD
-  const uint32_t IMAGE_WIDTH = 480;
-  const uint32_t IMAGE_HEIGHT = 272;
-  const uint32_t FRAMEBUFFER_ADDR = 0xC0000000;
-  
-  // Get pointer to framebuffer
-  uint16_t* framebuffer = (uint16_t*)FRAMEBUFFER_ADDR;
-  
-  // Copy the image data to framebuffer (480x272 pixels)
-  const uint32_t total_pixels = IMAGE_WIDTH * IMAGE_HEIGHT;
-  const uint32_t total_bytes = total_pixels * sizeof(uint16_t);
-  memcpy(framebuffer, image_data, total_bytes);
-  
-  // Enable LTDC display
-  HAL_LTDC_SetAddress(&hltdc, FRAMEBUFFER_ADDR, 0);
-  
-  // Ensure LCD backlight is on (already set in GPIO init)
-  HAL_GPIO_WritePin(LCD_BL_CTRL_GPIO_Port, LCD_BL_CTRL_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(LCD_DISP_GPIO_Port, LCD_DISP_Pin, GPIO_PIN_SET);
+    // Display image on LCD
+    const uint32_t IMAGE_WIDTH = 480;
+    const uint32_t IMAGE_HEIGHT = 272;
+    const uint32_t FRAMEBUFFER_ADDR = 0xC0000000;
+    
+    // Get pointer to framebuffer
+    uint16_t* framebuffer = (uint16_t*)FRAMEBUFFER_ADDR;
+    
+    // Copy the image data to framebuffer (480x272 pixels)
+    const uint32_t total_pixels = IMAGE_WIDTH * IMAGE_HEIGHT;
+    const uint32_t total_bytes = total_pixels * sizeof(uint16_t);
+    memcpy(framebuffer, default_image_data, total_bytes);
+    
+    // Enable LTDC display
+    HAL_LTDC_SetAddress(&hltdc, FRAMEBUFFER_ADDR, 0);
+    
+    // Ensure LCD backlight is on (already set in GPIO init)
+    HAL_GPIO_WritePin(LCD_BL_CTRL_GPIO_Port, LCD_BL_CTRL_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(LCD_DISP_GPIO_Port, LCD_DISP_Pin, GPIO_PIN_SET);
 
   printf("Bootup complete\r\n");
   
